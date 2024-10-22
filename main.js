ebb214a150661fe1f2ef7de3f460c9d410664da0
ibacwc <kirzaka8@protonmail.com>
2024-10-22 12:25:43 +0300

    all the things she s aid all the things she said runnin thru my head runnin thru my head runnin thru my head....

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let set_input = document.createElement('input');
 set_input.classList.add("set");
-set_input.setAttribute('type', 'text');
+set_input.setAttribute('type', 'number');
+let existing_exercises = new Map();
 
 function add_set(div){
 	let sets_container = div.querySelector('.sets-container');
@@ -15,6 +16,8 @@ function rm_set(div){
 }
 function calc_stats(){
 	let exercises = document.getElementsByClassName("exercise");
+	let stats_cont = document.getElementById("stats");
+	stats_cont.innerHTML = "";
 	for(let i =0; i<exercises.length; ++i){
 		let exc = exercises[i];
 		let exc_name = exc.id;
@@ -28,14 +31,24 @@ function calc_stats(){
 			reps_done += rta;
 			
 		}
-		console.log(`did ${exc_name} for ${reps_done} reps`)
+		let p = document.createElement("p");
+		p.innerHTML = `did ${exc_name} for ${reps_done} reps`;
+		stats_cont.append(p);
 	}
 }
 function add_exercise(exc){
+	if (existing_exercises.get(exc)){
+		let tmp = existing_exercises.get(exc);
+		tmp.classList.add("flash");
+		tmp.addEventListener("animationend", function(e) {
+			tmp.classList.remove("flash");
+		})
+		return;
+	}
 	let exc_div = document.createElement('div');
 	exc_div.classList.add('exercise');
 	exc_div.innerHTML = '<div class="sets-container">\
-                <input class="set" type="text">\
+                <input class="set" type="number">\
             </div>\
             <div class="column">\
                 <button onclick="add_set(this.parentNode.parentNode)">Add Set</button>\
@@ -47,5 +60,6 @@ function add_exercise(exc){
 	exc_div.insertBefore(paragraph, exc_div.firstChild);
 	const main = document.getElementById('main');
 	main.appendChild(exc_div);
+	existing_exercises.set(exc, exc_div);
 
 }
