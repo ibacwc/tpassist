65ce9936f4c9d86af730c99f1bb8c4d768002472
ibacwc <kirzaka8@protonmail.com>
2024-11-02 15:46:30 +0200

    starting work on saving workouts

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let set_input = document.createElement('input');
 let existing_exercises = new Map();
-
+let can_save = true;
 const exc_container = document.getElementById('exercise-container');
 const input = document.getElementById("exercise-input");
 const ul = document.getElementById("exercises");
+const today = new Date().toISOString().slice(0,10);
 let li;
 let mouse="";
 let lut_exercises = {
@@ -14,14 +15,13 @@ let lut_exercises = {
 	triceps: ["Diamond push ups", "Sphinx push ups"],
 	legs: ["Squat", "Pistol squat", "Knee bends", "Sissy squat", "Shrimp squat", "Nordic curl", "Glute bridges", "Calf raises"]
 };
-
-
 function clamp(value, min, max){
 	return (value < min) ? min : (value > max) ? max : value;
 }
 function activate_input(){
 	if(ul.style.display == "none"){
 		ul.style.display = 'block';
+		input.value = "";
 	}
 }
 function deactivate_input(){
@@ -29,39 +29,6 @@ function deactivate_input(){
 		ul.style.display='none';
 	}
 }
-function start(){
-	set_input.classList.add("set");
-	set_input.setAttribute('type', 'number');
-	
-	ul.style.display = "none";
-
-	input.addEventListener('focusin', activate_input);
-	input.addEventListener('focusout', deactivate_input);
-	input.addEventListener('click', activate_input);
-	
-	for(let category in lut_exercises){
-		lut_exercises[category].forEach(element => {
-			let tmp = document.createElement("li");
-			tmp.value = element;
-			tmp.innerHTML = element;
-			tmp.muscles_worked = category;
-			ul.appendChild(tmp);
-		});
-	}
-
-	li = document.getElementsByTagName('li');
-	for(let i =0; i< li.length;++i){
-		li[i].addEventListener('click', function (){
-			input.value = li[i].innerHTML;
-			ul.style.display = "none";
-		});
-		li[i].addEventListener('mouseover', function(){mouse="li"});
-		li[i].addEventListener('mouseout', function(){mouse=""});
-	}
-	check_storage();
-}
-
-
 function add_set(div){
 	let sets_container = div.querySelector('.sets-container');
 	sets_container.appendChild(set_input.cloneNode(true));
@@ -75,36 +42,6 @@ function rm_set(div){
 		existing_exercises.delete(sets_container.parentNode.parentNode.id);
 		sets_container.parentNode.parentNode.remove();
 	}
-}
-function calc_stats(){
-	let exercises = document.getElementsByClassName("exercise");
-	let stats_cont = document.getElementById("stats");
-	stats_cont.innerHTML = "<p>This workout you've done:<p>";
-	for(let i =0; i<exercises.length; ++i){
-		let exc = exercises[i];
-		let exc_name = exc.id;
-		let reps_done = 0;
-		let rep_boxes = exc.getElementsByClassName("sets-container")[0].children;
-		for (let j=0; j<rep_boxes.length; ++j){
-			let rta = parseInt(rep_boxes[j].value, 0);
-			if (rta !== rta){ //MURDER JS
-				continue;
-			}
-			rta = clamp(rta, 0, rta);
-			rep_boxes[j].value=rta;
-			reps_done += rta;
-		}
-		let p = document.createElement("p");
-		p.innerHTML = `${exc_name} for ${reps_done} rep`;
-		if(reps_done != 1){
-			p.innerHTML+='s';
-		}
-		stats_cont.append(p);
-	}
-	if(stats_cont.children.length <= 2){
-		stats_cont.innerHTML += "<p>Nothing!</p>";
-	}
-	window.location.href='#stats';
 }
 function add_exercise(exc){
 	if(exc ==""){
@@ -144,9 +81,9 @@ function add_exercise(exc){
 function check_storage(){
 	if(typeof(Storage) == "undefined"){
 		alert("No web storage support!! You can use this site, but you can't save your progress.");
+		can_save = false;
 	}
 }
-
 //The thief who has no oppertunity to steal thinks he is an honest man...
 function find_exercise(){
 	let filter = input.value.toUpperCase();
@@ -161,4 +98,72 @@ function find_exercise(){
 }
 function select_exercise(exc){
 	input.value = exc.innerHTML;
+}
+function start(){
+	set_input.classList.add("set");
+	set_input.setAttribute('type', 'number');
+	
+	ul.style.display = "none";
+
+	input.addEventListener('focusin', activate_input);
+	input.addEventListener('focusout', deactivate_input);
+	input.addEventListener('click', activate_input);
+	
+	for(let category in lut_exercises){
+		lut_exercises[category].forEach(element => {
+			let tmp = document.createElement("li");
+			tmp.value = element;
+			tmp.innerHTML = element;
+			tmp.muscles_worked = category;
+			ul.appendChild(tmp);
+		});
+	}
+
+	li = document.getElementsByTagName('li');
+	for(let i =0; i< li.length;++i){
+		li[i].addEventListener('click', function (){
+			input.value = li[i].innerHTML;
+			ul.style.display = "none";
+		});
+		li[i].addEventListener('mouseover', function(){mouse="li"});
+		li[i].addEventListener('mouseout', function(){mouse=""});
+	}
+	check_storage();
+}
+function calc_stats(){
+	let exercises = document.getElementsByClassName("exercise");
+	let stats_cont = document.getElementById("stats");
+	stats_cont.innerHTML = "<p>This workout you've done:<p>";
+	let save_string="";
+	for(let i =0; i<exercises.length; ++i){
+		let exc = exercises[i];
+		let exc_name = exc.id;
+		let reps_done = 0;
+		let rep_boxes = exc.getElementsByClassName("sets-container")[0].children;
+		save_string+=`${exc_name}`;
+		for (let j=0; j<rep_boxes.length; ++j){
+			let rta = parseInt(rep_boxes[j].value, 0);
+			if (rta !== rta){ //MURDER JS
+				rta=0;
+			}
+			rta = clamp(rta, 0, rta);
+			rep_boxes[j].value=rta;
+			reps_done += rta;
+			save_string+=`,${rta}`;
+		}
+		let p = document.createElement("p");
+		p.innerHTML = `${exc_name} for ${reps_done} rep`;
+		if(reps_done != 1){
+			p.innerHTML+='s';
+		}
+		stats_cont.append(p);
+		save_string+=`;\n`;
+	}
+	if(can_save && save_string != ""){
+		localStorage.setItem(today, save_string);
+	}
+	if(stats_cont.children.length <= 2){
+		stats_cont.innerHTML += "<p>Nothing!</p>";
+	}
+	window.location.href='#stats';
 }
