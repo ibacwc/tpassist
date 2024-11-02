d51f45fdc33bcffbb9a05ee016df1828fe0e9be7
ibacwc <kirzaka8@protonmail.com>
2024-11-02 14:07:49 +0200

    easier to add more exercises now...

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 let set_input = document.createElement('input');
-set_input.classList.add("set");
-set_input.setAttribute('type', 'number');
 let existing_exercises = new Map();
 
 const exc_container = document.getElementById('exercise-container');
 const input = document.getElementById("exercise-input");
 const ul = document.getElementById("exercises");
-const li = document.getElementsByTagName('li');
-ul.style.display = "none";
+let li;
+let mouse="";
+let lut_exercises = {
+	chest: ["Push ups", "Dips", "Ring flies"],
+	shoulders: ["Handstand push ups", "Pseudo push ups", "Pike push ups", "Rear delt flies", "Facepulls"],
+	back: ["Pull ups", "Chin ups", "Rows"],
+	biceps: ["Pelican curls", "Bicep curls"],
+	triceps: ["Diamond push ups", "Sphinx push ups"],
+	legs: ["Squat", "Pistol squat", "Knee bends", "Sissy squat", "Shrimp squat", "Nordic curl", "Glute bridges", "Calf raises"]
+};
 
+
+function clamp(value, min, max){
+	return (value < min) ? min : (value > max) ? max : value;
+}
 function activate_input(){
 	if(ul.style.display == "none"){
 		ul.style.display = 'block';
@@ -19,18 +29,38 @@ function deactivate_input(){
 		ul.style.display='none';
 	}
 }
+function start(){
+	set_input.classList.add("set");
+	set_input.setAttribute('type', 'number');
+	
+	ul.style.display = "none";
 
-input.addEventListener('focusin', activate_input);
-input.addEventListener('focusout', deactivate_input);
-input.addEventListener('click', activate_input);
-for(let i =0; i< li.length;++i){
-	li[i].addEventListener('click', function (){
-		input.value = li[i].innerHTML;
-		ul.style.display = "none";
-	});
-	li[i].addEventListener('mouseover', function(){mouse="li"});
-	li[i].addEventListener('mouseout', function(){mouse=""});
+	input.addEventListener('focusin', activate_input);
+	input.addEventListener('focusout', deactivate_input);
+	input.addEventListener('click', activate_input);
+	
+	for(let category in lut_exercises){
+		lut_exercises[category].forEach(element => {
+			let tmp = document.createElement("li");
+			tmp.value = element;
+			tmp.innerHTML = element;
+			tmp.muscles_worked = category;
+			ul.appendChild(tmp);
+		});
+	}
+
+	li = document.getElementsByTagName('li');
+	for(let i =0; i< li.length;++i){
+		li[i].addEventListener('click', function (){
+			input.value = li[i].innerHTML;
+			ul.style.display = "none";
+		});
+		li[i].addEventListener('mouseover', function(){mouse="li"});
+		li[i].addEventListener('mouseout', function(){mouse=""});
+	}
+	check_storage();
 }
+
 
 function add_set(div){
 	let sets_container = div.querySelector('.sets-container');
@@ -57,11 +87,12 @@ function calc_stats(){
 		let rep_boxes = exc.getElementsByClassName("sets-container")[0].children;
 		for (let j=0; j<rep_boxes.length; ++j){
 			let rta = parseInt(rep_boxes[j].value, 0);
-			if (rta !== rta){
+			if (rta !== rta){ //MURDER JS
 				continue;
 			}
+			rta = clamp(rta, 0, rta);
+			rep_boxes[j].value=rta;
 			reps_done += rta;
-			
 		}
 		let p = document.createElement("p");
 		p.innerHTML = `${exc_name} for ${reps_done} rep`;
@@ -93,7 +124,7 @@ function add_exercise(exc){
 	
 	'<div class="info">\
 		<div class="sets-container">\
-		<input class="set" type="number">\
+		<input class="set" type="number" min="0">\
 	</div>\
 	</div>\
 		<div class="column">\
@@ -119,11 +150,9 @@ function check_storage(){
 //The thief who has no oppertunity to steal thinks he is an honest man...
 function find_exercise(){
 	let filter = input.value.toUpperCase();
-
-
 	for (let i = 0; i< li.length; ++i){
 		let txt_val = li[i].innerHTML;
-		if(txt_val.toUpperCase().indexOf(filter) > -1){
+		if(txt_val.toUpperCase().indexOf(filter) > -1){ 
 			li[i].style.display="";
 		}else{
 			li[i].style.display="none";
